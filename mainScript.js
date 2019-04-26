fb3436bfae5de6a50959339b44d79a80557cc3ee
Johrten <JSternberg95@gmail.com>
2019-04-26 16:19:42 -0400

    Ready for public beta

--- mainScript.js
+++ mainScript.js
@@ -10,7 +10,10 @@
 // sheet.innerHTML = "._4299 {display:none;}"
 // document.body.appendChild(sheet)
 
-
-var sheet = document.createElement('style')
-sheet.innerHTML = "._666h._18vj._18vk._42ft {display:none} ._3vum {display:none} ._3w53 {display:none}"
-document.body.appendChild(sheet)
+if (!!document.querySelector('[title="Newsbook"]')) {}
+else {
+  var sheet = document.createElement('style')
+  sheet.setAttribute("title","Newsbook")
+  sheet.innerHTML = "._666h._18vj._18vk._42ft {display:none} ._3vum {display:none} ._3w53 {display:none}"
+  document.body.appendChild(sheet)
+}
--- popup.js
+++ popup.js
@@ -1,8 +1,16 @@
 let cleanFeed = document.getElementById('cleanFeed');
+let socialFeed = document.getElementById('socialFeed');
 
 cleanFeed.onclick = () => {
   chrome.tabs.query({active: true, currentWindow: true}, ()=> {
     chrome.tabs.executeScript(
         {file: 'mainScript.js'});
   });
-};
+}
+
+socialFeed.onclick = () => {
+  chrome.tabs.query({active: true, currentWindow: true}, ()=> {
+    chrome.tabs.executeScript(
+        {code: `document.querySelector('[title="Newsbook"]').remove()`});
+  });
+}

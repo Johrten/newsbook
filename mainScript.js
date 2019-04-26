a8878e1a177863ea5893b48277a8dc794bc03425
Johrten <JSternberg95@gmail.com>
2019-04-26 15:24:40 -0400

    Switched to a css based solution for spa persistance

--- mainScript.js
+++ mainScript.js
@@ -1,7 +1,16 @@
-social = document.getElementsByClassName("_4299")
-console.log(social)
+// social = document.getElementsByClassName("_4299")
+// console.log(social)
+//
+// for (let i = social.length - 1; i >= 0; i--) {
+//   console.log(social[i])
+//   social[i].remove()
+// }
 
-for (let i = social.length - 1; i >= 0; i--) {
-  console.log(social[i])
-  social[i].remove()
-}
+// var sheet = document.createElement('style')
+// sheet.innerHTML = "._4299 {display:none;}"
+// document.body.appendChild(sheet)
+
+
+var sheet = document.createElement('style')
+sheet.innerHTML = "._666h._18vj._18vk._42ft {display:none} ._3vum {display:none} ._3w53 {display:none}"
+document.body.appendChild(sheet)
--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
 let cleanFeed = document.getElementById('cleanFeed');
 
-cleanFeed.onclick = function(element) {
-  // let social = document.getElementsByClassName("_4299")
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+cleanFeed.onclick = () => {
+  chrome.tabs.query({active: true, currentWindow: true}, ()=> {
     chrome.tabs.executeScript(
-        tabs[0].id,
         {file: 'mainScript.js'});
   });
 };
